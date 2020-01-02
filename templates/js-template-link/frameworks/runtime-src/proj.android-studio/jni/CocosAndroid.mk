4641c58bb1673f680a8f5df4b7332d0c243fb5b8
吴智明 <wuzhiming@users.noreply.github.com>
2020-01-02 16:57:44 +0800

    Remove absolute path to avoid as build with absolute path (#2039)
    * remove absolute path to avoid as build with absolute path
    
    * remove href option by default to avoid build error when use cocos console

--- templates/js-template-link/frameworks/runtime-src/proj.android-studio/jni/CocosAndroid.mk
+++ templates/js-template-link/frameworks/runtime-src/proj.android-studio/jni/CocosAndroid.mk
@@ -10,9 +10,9 @@ ifeq ($(USE_ARM_MODE),1)
 LOCAL_ARM_MODE := arm
 endif
 
-LOCAL_SRC_FILES := $(LOCAL_PATH)/hellojavascript/main.cpp \
-				   $(LOCAL_PATH)/../../Classes/AppDelegate.cpp \
-				   $(LOCAL_PATH)/../../Classes/jsb_module_register.cpp \
+LOCAL_SRC_FILES := hellojavascript/main.cpp \
+				   ../../Classes/AppDelegate.cpp \
+				   ../../Classes/jsb_module_register.cpp \
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
