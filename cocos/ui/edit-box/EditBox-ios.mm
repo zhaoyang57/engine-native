900d6dc85b4cac95a73bf906d7c9156b590c140c
江战 <40414978+PatriceJiang@users.noreply.github.com>
2020-04-27 09:53:17 +0800

    [bugfix] correct utf8 encoding (#2394)
    * add UTF8LooseFix
    
    * fix include

--- cocos/ui/edit-box/EditBox-ios.mm
+++ cocos/ui/edit-box/EditBox-ios.mm
@@ -27,6 +27,7 @@
 #include "cocos/scripting/js-bindings/jswrapper/SeApi.h"
 #include "cocos/scripting/js-bindings/manual/jsb_global.h"
 
+
 #import <UIKit/UITextField.h>
 #import <UIKit/UITextView.h>
 
@@ -74,7 +75,7 @@ namespace
 {
     bool g_isMultiline = false;
     bool g_confirmHold = false;
-    int g_maxLength = INT_MAX;
+    unsigned int g_maxLength = INT_MAX;
     KeyboardEventHandler* g_keyboardHandler = nil;
 
     // "#1fa014", a color of dark green, was used for confirm button background
