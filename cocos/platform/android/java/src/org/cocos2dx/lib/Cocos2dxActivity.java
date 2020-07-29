9dc09137838980d956a56e8d18cd2721c26c660f
吴智明 <wuzhiming@users.noreply.github.com>
2020-07-29 14:56:17 +0800

    separate android and android instant launch activity (#2659)
    * add InstantActivity for instant template
    
    Signed-off-by: wuzhiming <wuzhiming.chukong@gmail.com>
    
    * fix immersive mode bug
    
    Signed-off-by: wuzhiming <wuzhiming.chukong@gmail.com>
    
    * add SYSTEM_UI_FLAG_IMMERSIVE_STICKY flag
    
    Signed-off-by: wuzhiming <wuzhiming.chukong@gmail.com>

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -39,6 +39,7 @@ import android.os.Bundle;
 import android.os.Message;
 import android.preference.PreferenceManager.OnActivityResultListener;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -284,7 +285,7 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 
-            flag |= immersiveSticky.getInt(null);
+            flag |= View.class.getDeclaredField("SYSTEM_UI_FLAG_IMMERSIVE_STICKY").getInt(null);
             View view = getWindow().getDecorView();
             view.setSystemUiVisibility(flag);
 
