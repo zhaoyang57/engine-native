7f6c5362824fec7f2c94e1270e8cc1fa2ae71993
zxx43 <zxx43@hotmail.com>
2021-07-09 10:09:33 +0800

    V2.4.6 video fix import hashmap (#3701)
    * fix remote video
    
    * add import

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxVideoView.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxVideoView.java
@@ -42,6 +42,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
 import java.util.Map;
 import java.nio.ByteBuffer;
 
